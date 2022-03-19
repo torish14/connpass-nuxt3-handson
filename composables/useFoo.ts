e9b93fe72e4769c63c35f8da810fdb32f2bea147
torish14 <nanamin.104@icloud.com>
2022-03-19 15:17:10 +0900

    📚 docs: composables は共通のステートやメソッドを使い回すために使用する

--- composables/useFoo.ts
+++ composables/useFoo.ts
@@ -1,5 +1,6 @@
 // 参考: https://vuejs-org.translate.goog/guide/reusability/composables.html?_x_tr_sl=en&_x_tr_tl=ja&_x_tr_hl=ja&_x_tr_pto=sc
 // 参考: https://v3.nuxtjs.org/docs/directory-structure/composables#composables-directory
+// 共通のステートやメソッドを使うためのコンポジュール
 export const useFoo = () => {
   return useState('foo', () => 'bar')
 }
