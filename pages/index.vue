c75ed0615d29b5a0ff8b70b2cd6dd79af496a777
torish14 <nanamin.104@icloud.com>
2022-03-19 14:40:43 +0900

    ✨ feat: vue 3 のサンプルファイルを作成

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+	setup() {
+		const name = ref('山田')
+		const testFunc = () => {
+			return '太郎'
+		}
+		return {
+			name,
+			testFunc
+		}
+	}
+})
+
+</script>
+
+<template>
+	<div>
+		テスト: {{ name }}  {{ testFunc() }}
+	</div>
+</template>
