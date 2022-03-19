18ccc7bc897cfac66359372cee8c603bfeb7cf24
torish14 <nanamin.104@icloud.com>
2022-03-19 15:29:04 +0900

    ✨ feat: クライアント、もしくはサーバーのみでプラグインの読み込みを制御するために、plugins ディレクトリを作成

---
+++ plugins/myPlugin.ts
@@ -0,0 +1,5 @@
+// 参考: https://v3.nuxtjs.org/docs/directory-structure/plugins#plugins-directory
+// 参考: https://zenn.dev/coedo/articles/route-middleware-nuxt3
+export default defineNuxtPlugin((nuxtApp) => {
+  console.log('myPlugin')
+})
