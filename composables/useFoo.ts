b4aa26d45f8eb305bc6b9d51fccc288ec8d6912c
torish14 <nanamin.104@icloud.com>
2022-03-19 15:15:40 +0900

    📚 docs: 参考 URL を追加

--- composables/useFoo.ts
+++ composables/useFoo.ts
@@ -1,3 +1,5 @@
+// 参考: https://vuejs-org.translate.goog/guide/reusability/composables.html?_x_tr_sl=en&_x_tr_tl=ja&_x_tr_hl=ja&_x_tr_pto=sc
+// 参考: https://v3.nuxtjs.org/docs/directory-structure/composables#composables-directory
 export const useFoo = () => {
   return useState('foo', () => 'bar')
 }
